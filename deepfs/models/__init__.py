53028ab7634f18514f216e59ed3ce6f4199fb88b
kkliang <1972475358@qq.com>
2026-04-11 18:58:03 +0800

    feat: export all 12 models from __init__

--- deepfs/models/__init__.py
+++ deepfs/models/__init__.py
@@ -1,17 +1,27 @@
 from deepfs.models.cae import ConcreteAutoencoderModel
 from deepfs.models.gumbel_sigmoid_gate import GumbelSigmoidGateModel
+from deepfs.models.gumbel_softmax_cae import GumbelSoftmaxGateConcreteModel
 from deepfs.models.gumbel_softmax_gate import GumbelSoftmaxGateModel
-from deepfs.models.hard_concrete_gate import HardConcreteGateModel
-from deepfs.models.ipcae import IndirectConcreteAutoencoderModel
 from deepfs.models.gumbel_softmax_ipcae import GumbelSoftmaxGateIndirectConcreteModel
+from deepfs.models.hard_concrete_cae import HardConcreteGateConcreteModel
+from deepfs.models.hard_concrete_gate import HardConcreteGateModel
+from deepfs.models.hard_concrete_ipcae import HardConcreteGateIndirectConcreteModel
+from deepfs.models.ipcae import IndirectConcreteAutoencoderModel
+from deepfs.models.stochastic_cae import StochasticGateConcreteModel
 from deepfs.models.stochastic_gate import StochasticGateModel
+from deepfs.models.stochastic_ipcae import StochasticGateIndirectConcreteModel
 
 __all__ = [
     "ConcreteAutoencoderModel",
     "GumbelSigmoidGateModel",
+    "GumbelSoftmaxGateConcreteModel",
     "GumbelSoftmaxGateModel",
     "GumbelSoftmaxGateIndirectConcreteModel",
+    "HardConcreteGateConcreteModel",
     "HardConcreteGateModel",
+    "HardConcreteGateIndirectConcreteModel",
     "IndirectConcreteAutoencoderModel",
+    "StochasticGateConcreteModel",
     "StochasticGateModel",
+    "StochasticGateIndirectConcreteModel",
 ]
